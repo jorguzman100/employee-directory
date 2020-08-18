2ab9202471ba24e776c57f19eb0184a167cdd1af
Jorge Guzman <jorguzman100@gmail.com>
2020-08-18 22:05:50 +0300

    Updates

---
+++ precache-manifest.0c0637f1632656b6eb7ba0d732565671.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8212b15a8306da1885ea6dcbfcdc72",
+    "url": "/19_employee_directory/index.html"
+  },
+  {
+    "revision": "9b849c98dd6027983f31",
+    "url": "/19_employee_directory/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "809bb33eefd9f8a0504f",
+    "url": "/19_employee_directory/static/css/main.61f65691.chunk.css"
+  },
+  {
+    "revision": "9b849c98dd6027983f31",
+    "url": "/19_employee_directory/static/js/2.c843d5e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19_employee_directory/static/js/2.c843d5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809bb33eefd9f8a0504f",
+    "url": "/19_employee_directory/static/js/main.32ca6a0b.chunk.js"
+  },
+  {
+    "revision": "e024c7726ada6875bf9f",
+    "url": "/19_employee_directory/static/js/runtime-main.eb686e11.js"
+  }
+]);
